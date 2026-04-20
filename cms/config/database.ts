059cdeb66592673a8e348e4894d14139e830038e
Mehmet Adıgüzel <1030321133@erciyes.edu.tr>
2026-04-20 04:52:11 +0300

    disable postgres ssl

--- cms/config/database.ts
+++ cms/config/database.ts
@@ -1,33 +1,17 @@
-import path from 'path';
 import type { Core } from '@strapi/strapi';
 
-// Force Node.js to ignore self-signed certificate errors for the entire process
-// This is necessary for some Strapi 5 + Managed DB configurations
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-
 const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Database => {
-  const client = 'postgres';
-
-  let connectionUrl = env('DATABASE_URL');
-  
-  if (connectionUrl && !connectionUrl.includes('sslmode=')) {
-    connectionUrl += connectionUrl.includes('?') ? '&sslmode=no-verify' : '?sslmode=no-verify';
-  }
-
   return {
     connection: {
-      client,
-      connection: (connectionUrl ? {
-        connectionString: connectionUrl,
-        ssl: { rejectUnauthorized: false }
-      } : {
+      client: 'postgres',
+      connection: {
         host: env('DATABASE_HOST'),
-        port: env.int('DATABASE_PORT', 25060),
+        port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME'),
         user: env('DATABASE_USERNAME'),
         password: env('DATABASE_PASSWORD'),
-        ssl: { rejectUnauthorized: false },
-      }) as any,
+        ssl: false,
+      },
       pool: {
         min: env.int('DATABASE_POOL_MIN', 2),
         max: env.int('DATABASE_POOL_MAX', 10),
